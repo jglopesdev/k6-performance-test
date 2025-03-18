09331b3efe856306e3865329e227865a60b05db0
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-18 09:22:47 -0300

    using html report for stress test

--- tests/hello.js
+++ tests/hello.js
@@ -1,6 +1,13 @@
 import http from 'k6/http';
 import {sleep, check} from 'k6';
 
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
+  }
 
 export default function() {
     const res = http.get('http://localhost:3333');
